ebdb127db6304787342d8ce36da8ffb29ba3b91c
Prayag Agrawal <prayag.a@PRAYAGAGRAWAL-408-AIR.local>
2022-09-08 14:43:55 +0530

    Modified for route apis

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect database
+connectDB();
 app.get('/', (req, res) => res.send('API Running !!'));
+
+//Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.port || 5000;
 
-app.listen(PORT, () => console.log(`Seerver started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
